d4fcd6a3966605bd7d157e8dc6804e790d9464de
mpodaniev <mpodanev@gmail.com>
2026-01-18 21:09:16 +0200

    dynamic image input and implement scrolling logic.

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+interface CarouselProps {
+  images: string[];
+}
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<CarouselProps> = ({ images }: CarouselProps) => {
+  const itemWidth = 130;
+  const frameSize = 3;
+  const step = 3;
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+  const maxIndex = images.length - frameSize;
+
+  const handleNextClick = () => {
+    const newIndex = currentIndex + step;
+
+    setCurrentIndex(Math.min(newIndex, maxIndex));
+  };
+
+  const handlePrevClick = () => {
+    const newIndex = currentIndex - step;
+
+    setCurrentIndex(Math.max(newIndex, 0));
+  };
+
+  const offset = currentIndex * itemWidth;
+
+  return (
+    <div className="Carousel">
+      <ul
+        className="Carousel__list"
+        style={{
+          transform: `translateX(-${offset}px)`,
+        }}
+      >
+        {images?.map((image, index) => (
+          <li key={`${image}-${index}`} className="Carousel__item">
+            <img src={image} alt={`${index + 1}`} />
+          </li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handlePrevClick}>
+        Prev
+      </button>
+      <button type="button" onClick={handleNextClick}>
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
